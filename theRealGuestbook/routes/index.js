9e32fe629503939874dbdbe812edc1f75344c336
Sarah <sarahsilva@live.com>
2016-11-07 12:57:20 -0600

    Displayed data from mongodb into guestbook page as a list.

--- theRealGuestbook/routes/index.js
+++ theRealGuestbook/routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var ObjectID = require('mongodb').ObjectID;
 var router = express.Router();
 
 /* GET landing page. */
@@ -11,15 +12,40 @@ router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Login' });
 });
 
+
 /* GET guestbook page. */
-router.get('/guestbook', function(req, res, next) {
-  res.render('guestbook', { title: 'Guestbook' });
+router.get('/guestbook', function(req, res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({}, {}, function(e,docs){
+    res.render('guestbook', {
+      "userlist" : docs
+    });
+  });
 });
+//
+// /* from working app... */
+// router.route('/guestbook')
+//   .get((req, res) => {
+//     var db = req.db;
+//     var collection = db.get('guestlist');
+//     collection.find({}, {}, function (e, docs) {
+//       res.render('guestbook', {
+//         guestlist: docs,
+//       });
+//     });
+//   })
+
+
 
 /* GET single view page. */
 router.get('/singleView', function(req, res, next) {
-  res.render('guestbook', { title: 'singleView' });
+  res.render('singleView', { title: 'singleView' });
 });
 
+/* GET new user page. */
+router.get('/newuser', function(req, res) {
+    res.render('newuser', { title: 'Add New User' });
+});
 
 module.exports = router;
